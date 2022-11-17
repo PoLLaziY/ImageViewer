1e4b4024c672ec9165212ee2330c98e18615f533
PoLLaziY <xlanzerx@gmail.com>
2022-11-17 14:41:31 +0300

    Create views and navigation Create skeleton of style

--- app/src/main/java/com/example/imageviewer/FavoriteFragment.kt
+++ app/src/main/java/com/example/imageviewer/FavoriteFragment.kt
@@ -5,8 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-class ImageViewPagerFragment : Fragment() {
+class FavoriteFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +15,6 @@ class ImageViewPagerFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.opened_image, container, false)
+        return inflater.inflate(R.layout.favorite_recycler_item, container, false)
     }
 }
--- app/src/main/java/com/example/imageviewer/MainActivity.kt
+++ app/src/main/java/com/example/imageviewer/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.imageviewer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.IdRes
+import androidx.navigation.fragment.findNavController
+import com.example.imageviewer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val navController by lazy {
+        supportFragmentManager.findFragmentById(R.id.nav_host_fragment)?.findNavController()
+    }
+
+    private var activeFragment = R.id.random
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.bottomNavigationBar.setOnItemSelectedListener { item ->
+            navigateTo(item.itemId)
+            true
+        }
+    }
+
+    private fun navigateTo(@IdRes navigationItemId: Int) {
+        //Обратное направление
+        when (activeFragment) {
+            R.id.search -> {
+                when (navigationItemId) {
+                    R.id.random -> navController?.navigate(R.id.action_search_to_random)
+                    R.id.favorite -> navController?.navigate(R.id.action_search_to_favorite)
+                }
+            }
+            R.id.favorite ->
+                when (navigationItemId) {
+                    R.id.random -> navController?.navigate(R.id.action_favorite_to_random)
+                    R.id.search -> navController?.navigate(R.id.action_favorite_to_search)
+                }
+            else -> {
+                when (navigationItemId) {
+                    R.id.favorite -> navController?.navigate(R.id.action_random_to_favorite)
+                    R.id.search -> navController?.navigate(R.id.action_random_to_search)
+                }
+            }
+        }
+        activeFragment = navigationItemId
     }
 }
